11ce5a7963e7a7176e51d7ed4d8180de702b7f24
kimjngyun <sanbada79@gist.ac.kr>
2020-12-06 19:28:52 +0900

    Generate maze by kruskal algorithm

--- games/example/kruskal.py
+++ games/example/kruskal.py
@@ -1,58 +1,260 @@
 import random
-import disjoint_set as ds
 
-class Cell:
-    def __init__(self):
-        self.top = True
-        self.bottom = True
-        self.left = True
-        self.right = True
+# Easy to read representation for each cardinal direction.
+N, S, W, E = ('n', 's', 'w', 'e')
 
-def init_variables(rows, cols):
+class Cell(object):
+    """
+    Class for each individual cell. Knows only its position and which walls are
+    still standing.
+    """
+    def __init__(self, x, y, walls):
+        self.x = x
+        self.y = y
+        self.walls = set(walls)
 
-    cell_list = []
-    for i in xrange(rows):
-        for j in xrange(cols):
-            cell_list.append((i, j))
+    def __repr__(self):
+        # <15, 25 (es  )>
+        return '<{}, {} ({:4})>'.format(self.x, self.y, ''.join(sorted(self.walls)))
 
-    cell_set = ds.disjoint_set(cell_list)
+    def __contains__(self, item):
+        # N in cell
+        return item in self.walls
 
-    cells = [[ Cell()
-        for y in range(cols) ]
-        for x in range(rows) ]
+    def is_full(self):
+        """
+        Returns True if all walls are still standing.
+        """
+        return len(self.walls) == 4
 
-    # print cells[2][0]
-    # print cells[1][0]
+    def _wall_to(self, other):
+        """
+        Returns the direction to the given cell from the current one.
+        Must be one cell away only.
+        """
+        assert abs(self.x - other.x) + abs(self.y - other.y) == 1, '{}, {}'.format(self, other)
+        if other.y < self.y:
+            return N
+        elif other.y > self.y:
+            return S
+        elif other.x < self.x:
+            return W
+        elif other.x > self.x:
+            return E
+        else:
+            assert False
 
-    edges = []
-    for x in xrange(rows):
-        for y in xrange(cols):
-            edges.append((x, y, 'L'))
-            edges.append((x, y, 'U'))
+    def connect(self, other):
+        """
+        Removes the wall between two adjacent cells.
+        """
+        other.walls.remove(other._wall_to(self))
+        self.walls.remove(self._wall_to(other))
 
-    return (edges, cell_set, cell_list, cells)
+class Maze(object):
+    """
+    Maze class containing full board and maze generation algorithms.
+    """
 
-""" Function that takes partially formed cells array and performs one step on it"""
-def generate_maze(edges, cell_set, cell_list, cells):
+    def __init__(self, width=20, height=20):
+        """
+        Creates a new maze with the given sizes, with all walls standing.
+        """
+        self.width = width
+        self.height = height
+        self.cells = []
+        for y in range(self.height):
+            for x in range(self.width):
+                self.cells.append(Cell(x, y, [N, S, E, W]))
 
-    # choose random wall
-    wall = random.choice(edges)
-    edges.remove(wall)
+    def __getitem__(self, index):
+        """
+        Returns the cell at index = (x, y).
+        """
+        x, y = index
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return self.cells[x + y * self.width]
+        else:
+            return None
 
-    x = wall[0]
-    y = wall[1]
+    def neighbors(self, cell):
+        """
+        Returns the list of neighboring cells, not counting diagonals. Cells on
+        borders or corners may have less than 4 neighbors.
+        """
+        x = cell.x
+        y = cell.y
+        for new_x, new_y in [(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]:
+            neighbor = self[new_x, new_y]
+            if neighbor is not None:
+                yield neighbor
 
-    if y > 0 and wall[2] == 'L' and cell_set.find((x, y)) != cell_set.find((x, y-1)):
-        cell_set.union((x, y), (x, y-1))
-        cells[x][y].left = False
-        cells[x][y-1].right = False
+    def _gid_matrix(self):
+        str_matrix = [[0] * (self.width * 2 + 1)
+                      for i in range(self.height * 2 + 1)]
 
-    if x > 0 and wall[2] == 'U' and cell_set.find((x, y)) != cell_set.find((x-1, y)):
-        cell_set.union((x, y), (x-1, y))
-        cells[x][y].top = False
-        cells[x-1][y].bottom = False
+        for cell in self.cells:
+            x = cell.x * 2 + 1
+            y = cell.y * 2 + 1
+            str_matrix[y][x] = 1
+            if N not in cell and y > 0:
+                str_matrix[y - 1][x + 0] = 1
+            if S not in cell and y + 1 < self.width:
+                str_matrix[y + 1][x + 0] = 1
+            if W not in cell and x > 0:
+                str_matrix[y][x - 1] = 1
+            if E not in cell and x + 1 < self.width:
+                str_matrix[y][x + 1] = 1
+        
+        #print(str_matrix)
+        
+        data_list=[]
+        
+        for x in range(self.width*2+1):
+            for y in range(self.height*2+1):
+                data=0
+                if str_matrix[x][y] == 1:
+                    data = 57
 
-    # if y == 0 and wall[2] == 'L' or x == 0 and wall[2] == 'U':
-        # edges.remove(wall)
+                else:
+                    data = 211 
+                    if x == 0:
+                        n = 1
+                    else:
+                        n = str_matrix[x-1][y]
 
-    return cells
+                    if x == self.height*2:
+                        s = 1
+                    else:
+                        s = str_matrix[x+1][y]
+
+                    if y == 0:
+                        w = 1
+                    else:
+                        w = str_matrix[x][y-1]
+
+                    if y == self.width*2:
+                        e = 1
+                    else:
+                        e = str_matrix[x][y+1]
+                    print(n, e, s, w)
+
+                    # append gid at array data
+                    if n == 0 and e == 0 and s == 0 and w == 0:
+                        data=241
+                    elif n == 0 and e == 0 and s == 0 and w == 1:
+                        data=225
+                    elif n == 0 and e == 0 and s == 1 and w == 0:
+                        data=239
+                    elif n == 0 and e == 0 and s == 1 and w == 1:
+                        data=223
+                    elif n == 0 and e == 1 and s == 0 and w == 0:
+                        data=233
+                    elif n == 0 and e == 1 and s == 0 and w == 1:
+                        data=217
+                    elif n == 0 and e == 1 and s == 1 and w == 0:
+                        data=231
+                    elif n == 0 and e == 1 and s == 1 and w == 1:
+                        data=215
+                    elif n == 1 and e == 0 and s == 0 and w == 0:
+                        data=237
+                    elif n == 1 and e == 0 and s == 0 and w == 1:
+                        data=221
+                    elif n == 1 and e == 0 and s == 1 and w == 0:
+                        data=235
+                    elif n == 1 and e == 0 and s == 1 and w == 1:
+                        data=219
+                    elif n == 1 and e == 1 and s == 0 and w == 0:
+                        data=229
+                    elif n == 1 and e == 1 and s == 0 and w == 1:
+                        data=213
+                    elif n == 1 and e == 1 and s == 1 and w == 0:
+                        data=227
+                data_list.append(data)
+        print(data_list)
+        return data_list
+
+    def randomize(self):
+        """
+        Knocks down random walls to build a random perfect maze.
+        Algorithm from http://mazeworks.com/mazegen/mazetut/index.htm
+        """
+        cell_stack = []
+        cell = random.choice(self.cells)
+        n_visited_cells = 1
+
+        while n_visited_cells < len(self.cells):
+            neighbors = [c for c in self.neighbors(cell) if c.is_full()]
+            if len(neighbors):
+                neighbor = random.choice(neighbors)
+                cell.connect(neighbor)
+                cell_stack.append(cell)
+                cell = neighbor
+                n_visited_cells += 1
+            else:
+                cell = cell_stack.pop()
+
+    @staticmethod
+    def generate(width=20, height=20):
+        """
+        Returns a new random perfect maze with the given sizes.
+        """
+        m = Maze(width, height)
+        m.randomize()
+        return m
+                    
+
+class MazeGame(object):
+    """
+    Class for interactively playing random maze games.
+    """
+    def __init__(self, maze):
+        self.maze = maze or Maze.generate()
+
+    def _get_random_position(self):
+        """
+        Returns a random position on the maze.
+        """
+        return (random.randrange(0, self.maze.width),
+                random.randrange(0, self.maze.height))
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        width = int(sys.argv[1])
+        if len(sys.argv) > 2:
+            height = int(sys.argv[2])
+        else:
+            height = width
+    else:
+        width = 20
+        height = 20
+    map_width = width*2+1
+    map_height = height*2+1
+    f = open("games/example/map1.tmx", 'w')
+    f.write('<?xml version="1.0" encoding="UTF-8"?> \n')
+    f.write('<map version="1.0" orientation="orthogonal" width="')
+    f.write('%d" height="' % map_width)
+    f.write('%d" tilewidth="64" tileheight="64"> \n' % map_height)
+    f.write('<properties> \n')
+    f.write('<property name="name" value="Blackrock"/> \n')
+    f.write('</properties> \n')
+    f.write('<tileset firstgid="1" name="graphics" tilewidth="64"   tileheight="64"> \n')
+    f.write('<image source="graphics2x-basic.png" width="640"   height="1344"/> \n')
+    f.write('</tileset> \n')
+    f.write('<tileset firstgid="211" name="walls" tilewidth="64"    tileheight="64"> \n')
+    f.write('<image source="graphics2x-walls.png" width="128"   height="1024"/> \n')
+    f.write('</tileset> \n')
+    f.write('<layer name="Tile Layer 1" width="%d" height="' % map_width)
+    f.write('%d"> \n' % map_height)
+    f.write('<data> \n')
+
+    for i in Maze._gid_matrix(Maze.generate(width, height)):
+        data = '<tile gid="%d"/>\n' % i
+        f.write(data)
+    f.write('</data> \n')
+    f.write('</layer> \n')
+    f.write('</map> \n')
+
+    f.close()
